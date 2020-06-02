df75b46adb5a91c98157465c4365277470802002
RolandMcDoland <miki.franek@gmail.com>
2020-06-02 23:20:12 +0200

    Lab3 added

---
+++ Lab3/nodetodo/public/core.js
@@ -0,0 +1,117 @@
+var nodeTodo = angular.module("nodeTodo", []);
+
+function mainController($scope, $http) {
+  $scope.formData = {};
+
+  $scope.cos = "Ala ma kota";
+
+  // when landing on the page, get all todos and show them
+  $http
+    .get("/api/todos")
+    .success(function(data) {
+      $scope.todos = data;
+    })
+    .error(function(data) {
+      console.log("Error: " + data);
+    });
+
+    $http
+    .get("/api/todos/done")
+    .success(function(data) {
+      $scope.dones = data;
+    })
+    .error(function(data) {
+      console.log("Error: " + data);
+    });
+
+    $http
+    .get("/api/todos/pending")
+    .success(function(data) {
+      $scope.pending = data;
+    })
+    .error(function(data) {
+      console.log("Error: " + data);
+    });
+
+  // when submitting the add form, send the text to the node API
+  $scope.createTodo = function() {
+    $http
+      .post("/api/todos", $scope.formData)
+      .success(function(data) {
+        $("input").val("");
+        $scope.todos = data;
+
+        $http
+        .get("/api/todos/pending")
+        .success(function(data) {
+          $scope.pending = data;
+        })
+        .error(function(data) {
+          console.log("Error: " + data);
+        });
+      })
+      .error(function(data) {
+        console.log("Error: " + data);
+      });
+  };
+
+  // update a todo after checking it
+  $scope.updateTodo = function(id) {
+    $http
+    .put("/api/todos/" + id)
+    .success(function(data) {
+      $scope.todos = data;
+
+      $http
+      .get("/api/todos/done")
+      .success(function(data) {
+        $scope.dones = data;
+      })
+      .error(function(data) {
+        console.log("Error: " + data);
+      });
+
+      $http
+      .get("/api/todos/pending")
+      .success(function(data) {
+        $scope.pending = data;
+      })
+      .error(function(data) {
+        console.log("Error: " + data);
+      });
+    })
+    .error(function(data) {
+      console.log("Error: " + data);
+    });
+  };
+
+  // delete a todo after checking it
+  $scope.deleteTodo = function(id) {
+    $http
+      .delete("/api/todos/" + id)
+      .success(function(data) {
+        $scope.todos = data;
+
+        $http
+        .get("/api/todos/done")
+        .success(function(data) {
+          $scope.dones = data;
+        })
+        .error(function(data) {
+          console.log("Error: " + data);
+        });
+    
+        $http
+        .get("/api/todos/pending")
+        .success(function(data) {
+          $scope.pending = data;
+        })
+        .error(function(data) {
+          console.log("Error: " + data);
+        });
+      })
+      .error(function(data) {
+        console.log("Error: " + data);
+      });
+  };
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A series of small tasks for frontend university classes
 ## Contents
 * **Lab1**- basic **HTML** and **CSS**, mainly focused on **layout cutting**
 * **Lab2**- basic **JavaScript**, with use of **jQuery**, development of Lab1, adding **simple effects** 
+* **Lab3**- simple application for **storing todos** in **Node.js** using **Express** framework, with **MongoDB** and use of **Mongoose**, core of app written by university teacher 
